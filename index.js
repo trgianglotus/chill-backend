f40f23a54b0aeaf2142a0a81574b9673a2634727
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-07-09 06:15:50 +0800

    creat schemas and resolvers

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { makeExecutableSchema } from 'graphql-tools';
+import path from 'path';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
 import models from './models';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schemas')));
+
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './resolvers'))
+);
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -13,13 +19,27 @@ const schema = makeExecutableSchema({
 });
 
 const app = express();
+
 const graphqlEndpoint = '/graphql';
+
 // bodyParser is needed just for POST.
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+app.use(
+  graphqlEndpoint,
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      models,
+      user: {
+        id: 1,
+      },
+    },
+  })
+);
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
 // Add { force: true } sync to drop tables on restart
-models.sequelize.sync({ force: true }).then(() => {
+models.sequelize.sync({ force: false }).then(() => {
   app.listen(8080);
 });
