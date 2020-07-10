b95109184dfc8fe7d813f54999b4b82f2023c5bd
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-07-10 10:43:05 +0800

    add user validation

--- schemas/user.js
+++ schemas/user.js
@@ -15,7 +15,12 @@ export default `
     getUser(id: Int!): User!
     allUsers: [User!]!
   }
+  type RegisterResponse {
+    ok: Boolean!
+    user: User
+    errors: [Error!]
+  }
   type Mutation {
-    register(username: String!, email: String!, password: String!): Boolean!
+    register(username: String!, email: String!, password: String!): RegisterResponse!
   }
 `;
