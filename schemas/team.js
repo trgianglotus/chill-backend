ebd6d1ebb9bd76e6f2474229fd477215639434e9
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-07-22 01:33:21 +0800

    add allTeams query

--- schemas/team.js
+++ schemas/team.js
@@ -1,5 +1,7 @@
 export default `
   type Team {
+    id: Int!
+    name: String!
     owner: User!
     members: [User!]!
     channels: [Channel!]!
@@ -7,6 +9,9 @@ export default `
   type CreateTeamResponse {
     ok: Boolean!
     errors: [Error!]
+  }
+  type Query {
+    allTeams: [Team!]!
   }
   type Mutation {
     createTeam(name: String!): CreateTeamResponse!
