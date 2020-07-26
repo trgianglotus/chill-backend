7be691ecc7b4f0087bac27297588406c673f1115
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-07-26 18:33:56 +0800

    add auth subscribtion

--- index.js
+++ index.js
@@ -75,7 +75,13 @@ app.use(
   }))
 );
 
-app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: graphqlEndpoint,
+    subscriptionsEndpoint: 'ws://localhost:8080/subscriptions',
+  })
+);
 
 const server = createServer(app);
 
@@ -88,6 +94,39 @@ models.sequelize.sync({ force: false }).then(() => {
         execute,
         subscribe,
         schema,
+        onConnect: async ({ token, refreshToken }, webSocket) => {
+          if (token && refreshToken) {
+            let user = null;
+            try {
+              const payload = jwt.verify(token, SECRET);
+              user = payload.user;
+            } catch (err) {
+              const newTokens = await refreshTokens(
+                token,
+                refreshToken,
+                models,
+                SECRET,
+                SECRET2
+              );
+              user = newTokens.user;
+            }
+            if (!user) {
+              throw new Error('Invalid auth tokens');
+            }
+
+            const member = await models.Member.findOne({
+              where: { teamId: 1, userId: user.id },
+            });
+
+            if (!member) {
+              throw new Error('Missing auth tokens!');
+            }
+
+            return true;
+          }
+
+          throw new Error('Missing auth tokens!');
+        },
       },
       {
         server,
