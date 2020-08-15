27810098bc44e69743ebb08fbd17b09309ba76a8
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-08-16 03:24:47 +0800

    grp dms

--- schemas/channel.js
+++ schemas/channel.js
@@ -5,13 +5,19 @@ export default `
     public: Boolean!
     messages: [Message!]!
     users: [User!]!
+    dm: Boolean!
   }
   type ChannelResponse {
     ok: Boolean!
     channel: Channel
     errors: [Error!]
   }
+  type DMChannelResponse {
+    id: Int!
+    name: String!
+  }
   type Mutation {
     createChannel(teamId: Int!, name: String!, public: Boolean=false, members: [Int!]=[]): ChannelResponse!
+    getOrCreateChannel(teamId: Int!, members: [Int!]!): DMChannelResponse!
   }
 `;
