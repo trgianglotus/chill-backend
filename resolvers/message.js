66ddb0a3549429ae8a17e228bca77a7a71cd79f0
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-08-16 02:44:55 +0800

    change url to 8080

--- resolvers/message.js
+++ resolvers/message.js
@@ -18,7 +18,7 @@ export default {
   },
   Message: {
     url: (parent) =>
-      parent.url ? `http://localhost:8081/${parent.url}` : parent.url,
+      parent.url ? `http://localhost:8080/${parent.url}` : parent.url,
     user: ({ user, userId }, args, { models }) => {
       if (user) {
         return user;
